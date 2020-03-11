63664f290eb7e46a0d9cfd671e36ce1cd25d3f9b
Pierre Bürki <pierre.burki@protonmail.com>
2020-03-11 16:47:02 +0100

    Change user model

--- marcdown_project/marcdown/models.py
+++ marcdown_project/marcdown/models.py
@@ -1,23 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import User as BaseUser
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(BaseUser):
-    favorites = models.ManyToManyField('Note')
+
+# Our user model has to extend the base, but this seems difficult from looking around
+# Instead, this is our profile model, which has a one to one relation
+# to the base model
+class Profile(models.Model):
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE, related_name='profile')
+    favorites = models.ManyToManyField('Note', blank=True)
 
     def __str__(self):
-        return self.username
+        return self.user.username
     
 
 class Note(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ownNotes')
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='ownNotes')
     public = models.BooleanField(default=True)
-    sharers = models.ManyToManyField(User)
+    sharers = models.ManyToManyField(User, blank=True)
 
     read_only = models.BooleanField(default=False)
 
     title = models.TextField()
-    tags = models.ManyToManyField('Tag')
+    tags = models.ManyToManyField('Tag', blank=True)
     content = models.TextField()
 
     def __str__(self):
