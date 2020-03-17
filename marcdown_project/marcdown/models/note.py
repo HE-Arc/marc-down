9be88dc0e4425b8a0a6642050392f6efaae0b545
Pierre Bürki <pierre.burki@protonmail.com>
2020-03-17 13:24:04 +0100

    Add note update using diffs

--- marcdown_project/marcdown/models/note.py
+++ marcdown_project/marcdown/models/note.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+import diff_match_patch as dmp_module
 class Note(models.Model):
     owner = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='ownNotes')
     public = models.BooleanField(default=True)
@@ -14,4 +15,19 @@ class Note(models.Model):
     def __str__(self):
         return self.title
 
+    def update(self, diff):
+        '''
+        Updates the content of the note using the given diff
+
+        Returns: boolean, true if the patch was successfully applied
+        '''
+        dmp = dmp_module.diff_match_patch()
+        patched_content, results = dmp.patch_apply(self.content, diff)
+
+        if all(results):
+            # patch is successfully applied
+            self.content = patched_content
+            return True
+        else:
+            return False
     # TODO: on save, update a bunch of stuff (where does it goooooo ?)
