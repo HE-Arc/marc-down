1b91780f3034940ecfeb32ca0c6f07afa4291d00
Pierre Bürki <pierre.burki@protonmail.com>
2020-03-24 10:55:41 +0100

    Update urls

--- marcdown_project/marcdown/urls.py
+++ marcdown_project/marcdown/urls.py
@@ -1,19 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 
 from . import views
 from . import api_views
 
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
+
+app_name = "marcdown"
 
 router = routers.DefaultRouter()
-router.register(r'session', api_views.SessionViewSet)
-router.register(r'user', api_views.UserViewSet)
-router.register(r'tags', api_views.TagsViewSet)
-router.register(r'note', api_views.NoteViewSet)
+router.register(r'user', api_views.UserViewSet, basename='user')
+#router.register(r'tags', api_views.TagsViewSet, basename='tags')
+#router.register(r'note', api_views.NoteViewSet, basename='note')
 
-urlpatterns = [
-    # path('', views.index, name='home'),
-
-    path('api/', include('rest_framework.urls', namespace='rest_framework'))
+urlpatterns = [    
+    path('', include(router.urls)),
 ]
--- marcdown_project/marcdown_project/urls.py
+++ marcdown_project/marcdown_project/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# for the time being
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/', include("marcdown.urls", namespace="marcdown")),
+    path('login/', auth_views.LoginView.as_view(), {'template_name' : 'registration/login.html'}),
 ]
+
