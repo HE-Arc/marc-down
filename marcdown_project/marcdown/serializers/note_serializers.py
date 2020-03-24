c198b0c870081daff3d10319ea7f6910cc1d4854
Pierre Bürki <pierre.burki@protonmail.com>
2020-03-24 12:02:34 +0100

    Change serializers into modelSerializers

---
+++ marcdown_project/marcdown/serializers/note_serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from marcdown.models import Note
+
+from .profile_serializer import SimpleProfileSerializer
+from .tag_serializer import TagSerializer
+
+class NoteBriefSerializer(serializers.ModelSerializer):
+    owner = SimpleProfileSerializer()
+    sharers = SimpleProfileSerializer(many=True)
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'owner', 'public', 'read_only', 'sharers', 'tags']
--- marcdown_project/marcdown/serializers/profile_serializer.py
+++ marcdown_project/marcdown/serializers/profile_serializer.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 
-class ProfileSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    # own_notes = NoteBriefSerializer()
-    # shared_notes = NoteBriefSerializer()
-    # faved_notes = NoteBriefSerializer()
-    # tags = TagSerializer()
+from marcdown.models import Profile
+
+class SimpleProfileSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='user.username')
+
+    class Meta:
+        model = Profile
+        fields = ['id', 'name']
+
+from .note_serializers import NoteBriefSerializer
+
+class ProfileSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='user.username')
+    own_notes = NoteBriefSerializer(many=True)
+    shared_notes = NoteBriefSerializer(many=True)
+    favorites = NoteBriefSerializer(many=True)
+    # tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Profile
+        fields = ['id', 'name', 'own_notes', 'shared_notes', 'favorites']
