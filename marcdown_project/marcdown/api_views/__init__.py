3f9b43158e6170545188da5ede64f13f9d8e3497
Pierre Bürki <pierre.burki@protonmail.com>
2020-03-24 10:55:18 +0100

    Remove useless api view

--- marcdown_project/marcdown/api_views/__init__.py
+++ marcdown_project/marcdown/api_views/__init__.py
@@ -1,4 +1,3 @@
-from .session import SessionViewSet
 from .user import UserViewSet
-from .tags import TagsViewSet
-from .notes import NotesViewSet
+#from .tags import TagsViewSet
+#from .notes import NotesViewSet
