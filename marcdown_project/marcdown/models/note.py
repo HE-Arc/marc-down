e54d6e5a9aa048ee2b8a2a9c045f13149487d770
Pierre Bürki <pierre.burki@protonmail.com>
2020-03-25 16:19:53 +0100

    Update note model with updating

--- marcdown_project/marcdown/models/note.py
+++ marcdown_project/marcdown/models/note.py
@@ -3,6 +3,7 @@ from django.db import models
 import diff_match_patch as dmp_module
 import re
 
+from . import Tag
 
 class Note(models.Model):
     owner = models.ForeignKey(
@@ -14,24 +15,41 @@ class Note(models.Model):
     read_only = models.BooleanField(default=False)
 
     title = models.TextField()
-    tags = models.ManyToManyField('Tag', blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     content = models.TextField()
 
     def __str__(self):
         return self.title
+    
+    def allow_update_from_user(self, profile):
+        if profile == self.owner:
+            return True
+        elif self.read_only:
+            return False
+        else:
+            return self.public or profile in self.sharers
 
-    def update(self, diff):
+    def update(self, patch_text):
         '''
-        Updates the content of the note using the given diff
+        Updates the content of the note using the given patch
 
         Returns: boolean, true if the patch was successfully applied
         '''
         dmp = dmp_module.diff_match_patch()
-        patched_content, results = dmp.patch_apply(self.content, diff)
+        patch = dmp.patch_fromText(patch_text)
+        patched_content, results = dmp.patch_apply(self.content, patch)
 
         if all(results):
             # patch is successfully applied
             self.content = patched_content
+
+            # update tags
+            self.tags.set([])
+            for new_tag in self.parse_tags():
+                tag_instance, dummy = Tag.objects.get_or_create(name=new_tag)
+                self.tags.add(tag_instance)
+                # TODO: erase useless tags ?
+            
             return True
         else:
             return False
@@ -44,7 +62,7 @@ class Note(models.Model):
 
         ###### tags: `tag1`, `tag2`, ...
 
-        Returns: array of strings, containing the tags
+        Returns: set of strings, containing the tags
         '''
         regex = r"^#{6} tags: (`[^`]+`(?:, `[^`]+`)*)$"
         for line in self.content.split('\n'):
@@ -52,7 +70,7 @@ class Note(models.Model):
             if match:
                 tags_string = match[0]
                 # split tags and trim ``
-                tags = list(
+                tags = set(
                     map(lambda tag: tag[1:-1], tags_string.split(", ")))
                 return tags
-        return []
+        return set()
