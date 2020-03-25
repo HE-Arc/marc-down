43e081a7c9c57d27baddf647009eab2c795f82d1
Pierre Bürki <pierre.burki@protonmail.com>
2020-03-25 16:20:26 +0100

    Create tags api endpoint

---
+++ marcdown_project/marcdown/api_views/tags.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse
+from rest_framework import viewsets
+from rest_framework.renderers import JSONRenderer
+
+from rest_framework.decorators import action
+
+from django.contrib.auth.models import User
+from marcdown.serializers import TagSerializer
+
+class TagViewSet(viewsets.ViewSet):
+    # get
+    # requires auth
+    def list(self, request):
+        user = request.user
+        if user.is_authenticated:
+            tags = user.profile.get_tags() # {tag_name : count}
+            return JsonResponse({tags : tags})
+        else:
+            # TODO: error : unauthenticated
+            pass
