c501409c53f67bf9ef98b7283b28cf744743d5de
Loïck Jeanneret <lock.jeanneret@gmail.com>
2020-03-31 14:56:05 +0200

    Add WIP register page
    Co-authored-by: Pierre Bürki <54360836+g-wizzy@users.noreply.github.com>

---
+++ marcdown_project/marcdown/views/register.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+# Based on https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
