026de6f05ff96f72739a0c5ddab9a8f01cc1a86b
Loïck Jeanneret <lock.jeanneret@gmail.com>
2020-03-31 11:38:46 +0200

    Fix missing root tag

--- marcdown_project/frontend/src/pages/Home.js
+++ marcdown_project/frontend/src/pages/Home.js
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
- 
+
 class Home extends Component {
   render() {
     return (
       <div>
-        <span>Tags</span>
-        <input type="text" />
         <div>
-          {/* tag list */}
+          <span>Tags</span>
+          <input type="text" />
+          <div>
+            {/* tag list */}
+          </div>
         </div>
-      </div>
-      <div>
-        <h1>My notes</h1>
-        <div></div>
-        <h1>Shared with me</h1>
-        <div></div>
+        <div>
+          <h1>My notes</h1>
+          <div></div>
+          <h1>Shared with me</h1>
+          <div></div>
+        </div>
       </div>
     );
   }
 }
- 
+
 export default Home;
