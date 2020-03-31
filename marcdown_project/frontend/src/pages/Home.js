7a1b4865aeba91e23473382b4cb11210cf9cd597
Loïck Jeanneret <lock.jeanneret@gmail.com>
2020-03-31 10:56:12 +0200

    Add better temporary content

--- marcdown_project/frontend/src/pages/Home.js
+++ marcdown_project/frontend/src/pages/Home.js
@@ -4,15 +4,17 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <span>Tags</span>
+        <input type="text" />
+        <div>
+          {/* tag list */}
+        </div>
+      </div>
+      <div>
+        <h1>My notes</h1>
+        <div></div>
+        <h1>Shared with me</h1>
+        <div></div>
       </div>
     );
   }
