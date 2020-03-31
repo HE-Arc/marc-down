a0402de7d1468f3612f13bf68ee8a67dd30b4cb8
Pierre Bürki <pierre.burki@protonmail.com>
2020-03-31 09:46:45 +0200

    Fuse user and profile for admin page

--- marcdown_project/marcdown/admin.py
+++ marcdown_project/marcdown/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import Profile, Note, Tag
 
-admin.site.register(Profile)
+# Inline Profile with User
+# https://docs.djangoproject.com/en/3.0/topics/auth/customizing/
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Note)
 admin.site.register(Tag)
