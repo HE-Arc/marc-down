d8427054a7b4a3e6891f7ca68a3ace2d638ae77c
Blatoy <lock.jeanneret@gmail.com>
2020-04-06 11:44:59 +0200

    Fix private route to work with the auth class

--- marcdown_project/frontend/src/components/PrivateRoute.js
+++ marcdown_project/frontend/src/components/PrivateRoute.js
@@ -1,34 +1,46 @@
 // Based on https://tylermcginnis.com/react-router-protected-routes-authentication/
 import React, { Component } from "react";
 import { Route, Redirect, BrowserRouter } from "react-router-dom";
+import { auth } from "../authentication.js";
 
-// TODO: Use API
-const fakeAuth = {
-    isAuthenticated: false,
-    authenticate(cb) {
-        this.isAuthenticated = true
-        setTimeout(cb, 100)
-    },
-    logout(cb) {
-        this.isAuthenticated = false
-        setTimeout(cb, 100)
-    }
-}
 
 class PrivateRoute extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            toLogin: false,
+            toMainPage: false
+        };
+    }
+
+    componentDidMount() {
+        auth.loggedIn().then((result) => {
+            if (result) {
+                this.setState({ toMainPage: true });
+            }
+            else {
+                this.setState({ toLogin: true });
+            }
+        });
+    }
+
     render(...rest) {
-        return (
-            fakeAuth.isAuthenticated === true ?
-                <Route {...rest} render={(props) => (
-                    <Component {...props} />
-                )} />
-                :
-                <BrowserRouter forceRefresh={true}> {/* Workaround because we use an external login page */}
-                    <Route basename="/">
-                        <Redirect to="/login" />
-                    </Route>
-                </BrowserRouter>
-        );
+        if (this.state.toMainPage === true) {
+            return <Route {...rest} render={(props) => (
+                <this.props.component {...props} />
+            )} />
+        }
+
+        if (this.state.toLogin) {
+            return (<BrowserRouter forceRefresh={true}> {/* Workaround because we use an external login page */}
+                <Route basename="/">
+                    <Redirect to="/login" />
+                </Route>
+            </BrowserRouter>)
+        }
+
+        return null;
     }
 }
 
