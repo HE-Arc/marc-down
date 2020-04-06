53a243508633a9b29c71fa218626c27f53732683
Blatoy <lock.jeanneret@gmail.com>
2020-04-06 23:55:56 +0200

    Add tag filtering

--- marcdown_project/frontend/src/pages/Home.js
+++ marcdown_project/frontend/src/pages/Home.js
@@ -51,22 +51,45 @@ class Home extends Component {
     this.setState({ tagSearchValue: tagName });
   }
 
+  _searchTagFilter(card) {
+    const searchValue = this.state.tagSearchValue.toLowerCase();
+    if (searchValue === "") {
+      return true;
+    }
+    else {
+      for (let i = 0; i < card.tags.length; i++) {
+        const tagName = card.tags[i].name.toLowerCase();
+        if (tagName.includes(searchValue) || searchValue.includes(tagName)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
   render() {
     return (
       <div>
         <div id="tags-container">
           <span id="tags-header">Tags</span>
-          <input value={this.state.tagSearchValue} type="text" placeholder="Filter by tags" onChange={() => {}}/>
+          <input value={this.state.tagSearchValue} type="text" placeholder="Filter notes by tags" onChange={(e) => {
+            this.setState({ tagSearchValue: e.target.value });
+          }} />
           <div id="tags-list">
+            {/* Default tag = clear */}
+            <TagListItem clickAction={() => { this._setSearchItem("") }} key={-1} count={this.state.ownCards.length + this.state.sharedCards.length}>
+              All notes
+            </TagListItem>
             {Object.keys(this.state.tags).map((key, index) =>
-              <TagListItem clickAction={(tagName) => {this._setSearchItem(tagName)}} key={index} count={this.state.tags[key]}>{key}</TagListItem>
+              <TagListItem clickAction={(tagName) => { this._setSearchItem(tagName) }} key={index} count={this.state.tags[key]}>{key}</TagListItem>
             )}
           </div>
         </div>
         <div id="note-container">
           <h1>My notes</h1>
           <div>
-            {this.state.ownCards.map((card, key) =>
+            {this.state.ownCards.filter((card) => this._searchTagFilter(card)).map((card, key) =>
               <Card key={key} id={card.id} tags={card.tags} stared={true} owner={card.owner.name}>{card.title || "Untitled"}</Card>
             )}
           </div>
