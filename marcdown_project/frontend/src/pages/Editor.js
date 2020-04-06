867bcb0ca185d7e8dff2ad93df59887996d2b83e
Blatoy <lock.jeanneret@gmail.com>
2020-04-06 13:01:57 +0200

    Add reactmarkdown

--- marcdown_project/frontend/src/pages/Editor.js
+++ marcdown_project/frontend/src/pages/Editor.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
-import { UnControlled as CodeMirror } from 'react-codemirror2'
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
-import '../../static/styles/code-mirror.css'
+import { UnControlled as CodeMirror } from "react-codemirror2"
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "../../static/styles/code-mirror.css"
+
+import ReactMarkdown from "react-markdown"
+
+const input = "# This is a header\n\nAnd this is a paragraph. TODO: Parse from codemirror"
 
 class Editor extends Component {
   render() {
@@ -12,8 +16,8 @@ class Editor extends Component {
           <CodeMirror
             value={"Editing note with ID:" + this.props.match.params.id}
             options={{
-              mode: 'xml',
-              theme: 'material',
+              mode: "xml",
+              theme: "material",
               lineNumbers: true,
             }}
             onChange={(editor, data, value) => {
@@ -21,7 +25,7 @@ class Editor extends Component {
           />
         </div>
         <div id="md-render">
-            
+          <ReactMarkdown source={input} />
         </div>
       </div>
     );
