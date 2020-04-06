a1337efbea29edb70d01f65753afe8d3b40aec57
Blatoy <lock.jeanneret@gmail.com>
2020-04-06 12:53:27 +0200

    Add codemirror

--- marcdown_project/frontend/src/pages/Editor.js
+++ marcdown_project/frontend/src/pages/Editor.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
+import { UnControlled as CodeMirror } from 'react-codemirror2'
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import '../../static/styles/code-mirror.css'
 
 class Editor extends Component {
   render() {
     return (
       <div>
-        <div>
-          <div></div>
-          <textarea value={"Editing note with ID:" + this.props.match.params.id} readOnly></textarea>
+          <CodeMirror
+            value={"Editing note with ID:" + this.props.match.params.id}
+            options={{
+              mode: 'xml',
+              theme: 'material',
+              lineNumbers: true,
+            }}
+            onChange={(editor, data, value) => {
+            }}
+          />
         </div>
         <div></div>
       </div>
