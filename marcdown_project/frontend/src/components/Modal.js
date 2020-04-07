6c373bce41b38fd9a16168b3e5dd772a1db9c380
Blatoy <lock.jeanneret@gmail.com>
2020-04-07 17:23:23 +0200

    Add modal component

---
+++ marcdown_project/frontend/src/components/Modal.js
@@ -0,0 +1,40 @@
+
+import React, { Component } from "react";
+
+export default class Modal extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            displayed: false
+        };
+    }
+
+    display() {
+        this.setState({
+            displayed: true
+        });
+    }
+
+    hide() {
+        this.setState({
+            displayed: false
+        });
+    }
+
+    hideOnClick(e) {
+        if (e.target.classList.contains("modal-background")) {
+            this.setState({
+                displayed: false
+            });
+        }
+    }
+
+    render(...rest) {
+        return (
+            this.state.displayed ? <div onClick={(e) => { this.hideOnClick(e); }} className="modal-background">
+                <div className="modal">{this.props.children}</div>
+            </div> : null
+        );
+    }
+}
